1f2476e6dc5d3c602460577e42f8b447c12f1583
marselek <marselek@foi.hr>
2016-08-16 01:28:52 +0200

    Promjene za Help i poboljšanje interface-a
    Promjenjeno da se KolnikAppHelp.chm file dohvaća sa relativnih adresa, a
    ne sa statičkih kako je bilo do sad. Poboljšanja vizualnog izgleda UI-a
    sa labelama na MouseEnter i MouseLeave

--- KolnikApp/obrazac.cs
+++ KolnikApp/obrazac.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,12 +16,15 @@ namespace KolnikApp
         public obrazac()
         {
             InitializeComponent();
-        }
-
+        }       
         string[] tablice = new string[17] { "Artikl", "Rabat", "Zaposlen", "Poduzeće", "Vozi", "Vozilo", "Temeljnica", "Narudžbenica", "Zaposlenik", "Radi", "Korisnički račun", "Račun", "Otpremnica", "Tablična privilegija", "Gradilište", "Nalog za proizvodnju", "Radno mjesto" };
+
+        String exeDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+
         private void HelpSlika_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Help.ShowHelp(this, "file://C:\\Users\\Smrad\\Documents\\Visual Studio 2015\\Projects\\KolnikApp\\KolnikApp\\Resources\\KolnikAppHelp.chm");
+            String HelpFilepath = "file://" + Path.Combine(exeDirectory, "KolnikAppHelp.chm");
+            Help.ShowHelp(this, HelpFilepath);     
         }
 
         private void izgradiMeni()
@@ -38,9 +42,62 @@ namespace KolnikApp
 
         private void HomeSlika_MouseEnter(object sender, EventArgs e)
         {
-            Label LabelaHome = new Label();
-            LabelaHome.Text = "Početna";
-            LabelaHome.Show();
+            LabelaPocetna.Show();
+        }
+
+        private void HomeSlika_MouseLeave(object sender, EventArgs e)
+        {
+            LabelaPocetna.Hide();
+        }
+
+        private void NatragSlika_MouseEnter(object sender, EventArgs e)
+        {
+            LabelaNatrag.Show();
+        }
+
+        private void NatragSlika_MouseLeave(object sender, EventArgs e)
+        {
+            LabelaNatrag.Hide();
+        }
+
+        private void CreateSlika_MouseEnter(object sender, EventArgs e)
+        {
+            LabelaNovi.Show();
+        }
+
+        private void CreateSlika_MouseLeave(object sender, EventArgs e)
+        {
+            LabelaNovi.Hide();
+        }
+
+        private void UpdateSlika_MouseEnter(object sender, EventArgs e)
+        {
+            LabelaUpdate.Show();
+        }
+
+        private void UpdateSlika_MouseLeave(object sender, EventArgs e)
+        {
+            LabelaUpdate.Hide();
+        }
+
+        private void DeleteSlika_MouseEnter(object sender, EventArgs e)
+        {
+            LabelaIzbrisi.Show();
+        }
+
+        private void DeleteSlika_MouseLeave(object sender, EventArgs e)
+        {
+            LabelaIzbrisi.Hide();
+        }
+
+        private void HelpSlika_MouseEnter(object sender, EventArgs e)
+        {
+            LabelaPomoc.Show();
+        }
+
+        private void HelpSlika_MouseLeave(object sender, EventArgs e)
+        {
+            LabelaPomoc.Hide();
         }
     }
 }
