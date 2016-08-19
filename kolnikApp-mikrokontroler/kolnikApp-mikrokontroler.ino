5a7383a2370b85e9338252ab48ded4718fa54e33
zeko868 <zeko868@hotmail.com>
2016-08-19 23:51:22 +0200

    Dodana detekcija dolaska/odlaska vozila kod mikrokontrolera
    Programu mikrokontrolera je dodana mogućnost prepoznavanja kada je neko
    vozilo stiglo pred otpremište (korištenjem prijemnika infracrvene
    svjetlosti (IR) ugrađenog na mikrokontroler s jedne strane puta, te IR
    LED diode spojene na bateriju s druge strane).

--- kolnikApp-mikrokontroler/kolnikApp-mikrokontroler.ino
+++ kolnikApp-mikrokontroler/kolnikApp-mikrokontroler.ino
@@ -1,58 +1,135 @@
+#include "Arduino-IRremote-master/IRremote.h"
+#include "Arduino-IRremote-master/IRremoteInt.h"
+#include "Arduino-IRremote-master/ir_Lego_PF_BitStreamEncoder.h"
 #include "MAX6675-library-master/max6675.h"
+
+//const uint8_t thermoGNDPin = 2;
+//const uint8_t thermoVCCPin = 3;
+const uint8_t thermoSOPin = 8;
+const uint8_t thermoCSPin = 9;
+const uint8_t thermoSCKPin = 10;
+const uint8_t IRRecvPin = 4;
+const uint32_t frequencyOfInfraredLEDInHertz = 38000;
+const uint32_t timePeriodBetween2AdjacentLEDEmissionsInMicroseconds = (1. / frequencyOfInfraredLEDInHertz) * 1000000;
+
+unsigned long startTimeOfCurrentMeasuring = 0;
 String inputString = "";
-const int thermo_gnd_pin = 2;
-const int thermo_vcc_pin = 3;
-const int thermo_so_pin = 4;
-const int thermo_cs_pin = 5;
-const int thermo_sck_pin = 6;
+MAX6675 thermocoupleHandler(thermoSCKPin, thermoCSPin, thermoSOPin);
+IRrecv irrecv(IRRecvPin);
+decode_results results;
+bool vehicleIsHere = false;
 
-MAX6675 thermocouple(thermo_sck_pin, thermo_cs_pin, thermo_so_pin);
+inline bool IsObstacleBetweenDiodeAndReceiver()
+{
+	return !irrecv.decode(&results);
+}
 
-void setup() {
+void ListenForInfraredSignal()
+{
+	//-ako detektor nije primio signal && prisutnostVozila==false
+	//  -odspavaj otprilike jednu sekundu (na taj nacin se ignoriraju smetnje poput prolaska covjeka ili ptice izmedju lasera i detektora)
+	//  -ako detektor opet nije primio signal
+	//    -prisutnostVozila = true
+	//    -Serial.println("poruka")
+	//-inace ako je detektor primio signal && prisutnostVozila==true
+	//  -odspavaj otprilike dvije sekunde (na taj nacin se ignorira moguc prolazak snopa svjetlosti lasera kroz kabinu vozila ili prolaz svjetlosti kroz prostor izmedju kabine i sanduka)
+	//  -ako je detektor opet primio signal
+	//    -prisutnostVozila = false
+	if (!startTimeOfCurrentMeasuring)
+	{
+		if (!vehicleIsHere)
+		{
+			if (IsObstacleBetweenDiodeAndReceiver())
+			{
+				startTimeOfCurrentMeasuring = millis();
+			}
+		}
+		else
+		{
+			if (!IsObstacleBetweenDiodeAndReceiver())
+			{
+				startTimeOfCurrentMeasuring = millis();
+			}
+		}
+	}
+	else
+	{
+		if (!vehicleIsHere)
+		{
+			if (millis() - startTimeOfCurrentMeasuring >= 1000)
+			{
+				startTimeOfCurrentMeasuring = 0;
+				delayMicroseconds(timePeriodBetween2AdjacentLEDEmissionsInMicroseconds);
+				if (IsObstacleBetweenDiodeAndReceiver())
+				{
+					vehicleIsHere = true;
+					Serial.println("NOTIFY");
+				}
+			}
+		}
+		else
+		{
+			if (millis() - startTimeOfCurrentMeasuring >= 2000)
+			{
+				irrecv.resume();
+				startTimeOfCurrentMeasuring = 0;
+				delayMicroseconds(timePeriodBetween2AdjacentLEDEmissionsInMicroseconds);
+				if (!IsObstacleBetweenDiodeAndReceiver())
+				{
+					vehicleIsHere = false;
+					irrecv.resume();
+				}
+			}
+		}
+	}
+}
+
+void setup()
+{
 	Serial.begin(9600);
-	while (!Serial) {
+	while (!Serial)
+	{
 		;
 	}
+	irrecv.enableIRIn();
 	inputString.reserve(200);
-	pinMode(thermo_vcc_pin, OUTPUT);
-	pinMode(thermo_gnd_pin, OUTPUT);
-	digitalWrite(thermo_vcc_pin, HIGH);
-	digitalWrite(thermo_gnd_pin, LOW);
+	/*
+	pinMode(thermoVCCPin, OUTPUT);
+	pinMode(thermoGNDPin, OUTPUT);
+	digitalWrite(thermoVCCPin, HIGH);
+	digitalWrite(thermoGNDPin, LOW);
+	*/
 }
 
-void loop() {
-
+void loop()
+{
+	ListenForInfraredSignal();
 }
 
-void VehicleHasArrived() {
-	//-ako je detektor primio signal && prisutnostVozila==false
-	//  -odspavaj otprilike jednu sekundu (na taj nacin se ignoriraju smetnje poput prolaska covjeka ili ptice izmedju lasera i detektora)
-	//  -ako je opet detektor primio signal
-	//    -prisutnostVozila = true
-	//    -Serial.println(
-	//-inace ako detektor nije primio signal && prisutnostVozila==true
-	//  -odspavaj otprilike dvije sekunde (na taj nacin se ignorira moguc prolazak snopa svjetlosti lasera kroz kabinu vozila ili prolaz svjetlosti kroz prostor izmedju kabine i sanduka)
-	//  -ako detektor opet nije primio signal
-	//    -prisutnostVozila = false
-}
-
-void serialEvent() {
+void serialEvent()
+{
 	ReadLineOverSerialCommunication();
 }
 
-void ReadLineOverSerialCommunication() {
-	while (Serial.available()) {
+void ReadLineOverSerialCommunication()
+{
+	while (Serial.available())
+	{
 		char primljenoSlovo = (char)Serial.read();
-		if (primljenoSlovo != '\n') {
+		if (primljenoSlovo != '\n')
+		{
 			inputString += primljenoSlovo;
 		}
-		else {
-			if (DoesStringStartWithNeedle(inputString.c_str(), "temperatura")) {
+		else
+		{
+			if (DoesStringStartWithNeedle(inputString.c_str(), "temperatura"))
+			{
 				String newNeedle = "OIB=";
 				int newNeedlePos = inputString.indexOf(newNeedle);
-				if (newNeedlePos != -1) {
+				if (newNeedlePos != -1)
+				{
 					String oibPrimatelja = inputString.substring(newNeedlePos + newNeedle.length());
-					Serial.println("TMP=" + String(thermocouple.readCelsius()) + "&OIB=" + oibPrimatelja);
+					Serial.println("TMP=" + String(thermocoupleHandler.readCelsius()) + "&OIB=" + oibPrimatelja);
 				}
 			}
 			inputString = "";
@@ -61,7 +138,8 @@ void ReadLineOverSerialCommunication() {
 	}
 }
 
-bool DoesStringStartWithNeedle(const char* pre, const char* str) {
+bool DoesStringStartWithNeedle(const char* pre, const char* str)
+{
 	size_t lenpre = strlen(pre), lenstr = strlen(str);
 	return lenstr < lenpre ? false : strncmp(pre, str, lenpre) == 0;
 }
